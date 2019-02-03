5646f57ba266723dc813c306c87035637fadda2e
Itamar Rocha <itarocha@gmail.com>
2019-02-03 00:38:19 -0200

    Desistindo do Bootstrap Vue; Indo para o Element-UI

---
+++ betesda-app/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-tabs type="border-card">
+      <el-tab-pane label="User">User</el-tab-pane>
+      <el-tab-pane label="Config">Config</el-tab-pane>
+      <el-tab-pane label="Role">Role</el-tab-pane>
+      <el-tab-pane label="Task">Task</el-tab-pane>
+    </el-tabs>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Bem vindo ao Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
